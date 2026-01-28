e734c77c9ca1bdd2ac66261a58cd2d0ac66385e0
Cascade-Owl <bryart330@gmail.com>
2026-01-29 00:26:24 +0800

    Fixed bug

--- app.py
+++ app.py
@@ -1,11 +1,12 @@
-from flask import Flask, request,url_for, render_template, jsonify
+from flask import Flask, request, url_for, render_template, jsonify
 import requests
 import os
 import google.generativeai as genai
 
-app=Flask(__name__)
-api=os.getenv("GEMINI_API_KEY")
-model = genai.GenerativeModel("gemini-1.5-flash")
+
+app = Flask(__name__)
+api = os.getenv("GEMINI_API_KEY")
+model = genai.GenerativeModel("gemini-2.5-flash")
 GEMINI_API_URL = "AIzaSyCx6yXtGV6XMlTb4WgF6gsyQtU6A3V732A"
 genai.configure(api_key=GEMINI_API_URL)
 
@@ -15,16 +16,14 @@ def home():
     return render_template("index.html")
 
 
-@app.route("/chat" , methods=["POST", "GET"])
+@app.route("/botchat", methods=["POST", "GET"])
 def chat():
     userint = request.json.get("message")
 
-    data = {userint : "inputText"}
     response = model.generate_content(userint)
-    reply = {response : "botResponse"}
+    reply = {"response": response.text}
     return jsonify(reply)
 
 
 if __name__ == "__main__":
     app.run(debug=True)
-    
