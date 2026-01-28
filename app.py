396b8084b6e201b414ce326fe51da1e838efeb14
Jeremydaniel09 <jeremydaniel091105@gmail.com>
2026-01-28 23:58:53 +0800

    Implemented backend

--- app.py
+++ app.py
@@ -1,12 +1,30 @@
-from flask import Flask, render_template
+from flask import Flask, request,url_for, render_template, jsonify
+import requests
+import os
+import google.generativeai as genai
 
-app = Flask(__name__)
+app=Flask(__name__)
+api=os.getenv("GEMINI_API_KEY")
+model = genai.GenerativeModel("gemini-1.5-flash")
+GEMINI_API_URL = "AIzaSyCx6yXtGV6XMlTb4WgF6gsyQtU6A3V732A"
+genai.configure(api_key=GEMINI_API_URL)
 
 
-@app.route("/", methods=["POST", "GET"])
+@app.route("/")
 def home():
     return render_template("index.html")
 
 
+@app.route("/chat" , methods=["POST", "GET"])
+def chat():
+    userint = request.json.get("message")
+
+    data = {userint : "inputText"}
+    response = model.generate_content(userint)
+    reply = {response : "botResponse"}
+    return jsonify(reply)
+
+
 if __name__ == "__main__":
     app.run(debug=True)
+    
