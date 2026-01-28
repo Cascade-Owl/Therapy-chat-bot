525229ca8b2c452fdbd5d2c73e351c07e7776ff2
Cascade-Owl <bryart330@gmail.com>
2026-01-28 19:04:28 +0800

    Made frontend dynamic

---
+++ static/script.js
@@ -0,0 +1,43 @@
+const chatBubble = document.getElementById("chatBubble");
+const chatForm = document.getElementById("chatForm");
+const userInput = document.getElementById("userInput");
+
+function appendMessage(sender, text) {
+    const chatDiv = document.createElement("div");
+
+    chatDiv.classList.add("message", sender);
+
+    chatDiv.innerText = text;
+
+    chatBubble.appendChild(chatDiv);
+
+    chatDiv.scrollTop = chatDiv.scrollHeight;
+}
+
+chatForm.addEventListener("submit", async function (event){
+    event.preventDefault();
+
+    let inputText = userInput.value;
+
+    appendMessage("user", inputText);
+
+    if (!(inputText)) return;
+
+    inputText = "";
+
+    try{
+        const response = await fetch("/botchat", {
+            method: "POST",
+            headers: {"Content-type" : "application/json"},
+            body: JSON.stringify({message: inputText})
+    });
+
+        const responseData = await response.json();
+
+        appendMessage("bot", responseData);
+    }
+
+    catch(error){
+        appendMessage("bot", error);
+    }
+});
