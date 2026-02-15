4d2dfe315d6a52beab1acf44d93415e63afc1122
Jeremydaniel09 <jeremydaniel091105@gmail.com>
2026-02-16 00:56:01 +0800

    Added chatbot memory

--- app.py
+++ app.py
@@ -12,7 +12,8 @@ GEMINI_API_URL = os.getenv("GEMINI_API_URL")
 genai.configure(api_key=GEMINI_API_URL)
 model = genai.GenerativeModel("gemini-2.5-flash")
 
-
+chat_history=[]
+chat_historybot=[]
 @app.route("/")
 def home():
     return render_template("index.html")
@@ -22,12 +23,17 @@ def home():
 def chat():
     userint = request.json.get("message")
 
+
+
     botResponse = model.generate_content(
-        f"""You are a slightly serious, professional speaking, yet comedic and a humourous therapist, 
+        f"""{chat_history}/n You are a slightly serious, professional speaking, yet comedic and a humourous therapist, 
         if someone prompts for another topic unrelated to your therapist job, 
         tell them that you are a therapist only, and you can't do other unrelated topic but be sensitive, user says: {userint}"""
     )
     reply = {"response": botResponse.text}
+    chat_history.append({"role": "user", "parts": [str(userint)]})
+    chat_historybot.append({"role": "model", "parts": [botResponse.text]})
+
     return jsonify(reply)
 
 
